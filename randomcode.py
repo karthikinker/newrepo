7e10ca3d4e4f9c40f12447c51a9d0b0b610409d7
karthikinker <karthik.pillai@inkerrobotics.com>
2021-03-25 12:58:20 +0530

    random python list learning

--- randomcode.py
+++ randomcode.py
@@ -1,1 +1,13 @@
-print("india")
+#import os
+#os.system('cls' if os.name == 'nt' else 'clear')
+mylist = []
+mylist.append("1hoi")
+mylist.append(2)
+mylist.append(3)
+print(mylist[0]) # prints 1
+print(mylist[1]) # prints 2
+print(mylist[2]) # prints 3
+
+# prints out 1,2,3
+for x in mylist:
+    print(x)
